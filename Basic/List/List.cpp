2b3d6952a007939f7a4af972b631d56ad88f7f58
knibey <clevertea22@gmail.com>
2021-06-07 13:17:53 +0300

    new changes

---
+++ Basic/List/List.cpp
@@ -0,0 +1,53 @@
+List(int capacity=100, double multiplier=1.05) {
+        
+}
+
+~List() {}
+
+int size() const {
+
+}
+
+int max_size() const {
+
+}
+
+void erase(int index) {
+
+}
+
+void insert(int value, int index) {
+
+}
+
+int find(int value) const {
+
+}
+
+void push_back(int value) {
+
+}
+
+int pop_back() {
+
+}
+
+void sort() {
+
+}
+
+int operator[](int index) const {
+
+}
+
+bool operator==(const List& other) const {
+
+}
+
+bool operator!=(const List& other) const {
+
+}
+
+std::ostream& operator<<(std::ostream& out, const List& list) {
+
+}
