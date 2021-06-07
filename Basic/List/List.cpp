4b541959de136985b3a9f0bd2349ee231323d43f
knibey <clevertea22@gmail.com>
2021-06-07 13:52:27 +0300

    changes

--- Basic/List/List.cpp
+++ Basic/List/List.cpp
@@ -1,35 +1,63 @@
+#include "List.hpp"
+
 List(int capacity=100, double multiplier=1.05) {
-        
+        array = (int*)malloc(capacity*sizeof(int));
+
+        if ( array == NULL ) {
+                throw OutOfMemoryException();
+        }
 }
 
-~List() {}
+~List() {
+    free(array);
+}
 
 int size() const {
-
+    return this->current;
 }
 
 int max_size() const {
-
+    return this->capacity;
 }
 
 void erase(int index) {
-
+    this->index = 0;
 }
 
 void insert(int value, int index) {
-
+    this->[index]=value;
 }
 
 int find(int value) const {
-
+    return this->[index]=value;
 }
 
 void push_back(int value) {
+        int newCurrent = current + 1;
 
+        if ( newCurrent > capacity ) {
+                int newCapacity = capacity * multiplier;
+                int* newArray = (int*)realloc(array, newCapacity*sizeof(int));
+
+                if ( newArray == NULL ) {
+                        throw OutOfMemoryException();
+                }
+
+                capacity = newCapacity;
+                array = newArray;
+        }
+
+        array[current] = value;
+        current = newCurrent;
 }
 
 int pop_back() {
+        if ( current == 0 ) {
+                throw ZeroLenException();
+        }
 
+        current -= 1;
+        return array[current];
 }
 
 void sort() {
@@ -49,5 +77,12 @@ bool operator!=(const List& other) const {
 }
 
 std::ostream& operator<<(std::ostream& out, const List& list) {
+        int last = list.size() - 1;
 
+        for ( int i = 0; i < last; i++ ) {
+                out << list[i] << ' ';
+        }
+
+        out << list[last];
+        return out;
 }
